d889079a032195503e7054d9c87558db81bd0390
John Tyree <johntyree@gmail.com>
2009-08-29 02:17:33 -0400

    Database walks root dir and gathers path / mtime / size.

--- db.py
+++ db.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 
 from pysqlite2 import dbapi2 as sqlite
+import time, sys, os
 
-#connection = sqlite.connect('/tmp/test.db')
-connection = sqlite.connect(':memory:')
+connection = sqlite.connect('/tmp/test.db')
 cursor = connection.cursor()
-
-cursor.execute('CREATE TABLE names (id INTEGER PRIMARY KEY, name VARCHAR(50), email VARCHAR(50))')
-cursor.execute('INSERT INTO names VALUES (null, "John Doe", "jdoe@jdoe.zz")')
-cursor.execute('INSERT INTO names VALUES (null, "Mary Sue", "msue@msue.yy")')
+try:
+    cursor.execute('CREATE TABLE files (id INTEGER PRIMARY KEY, path VARCHAR(255), timestamp DECIMAL(12), size INTEGER(12))')
+except (sqlite.OperationalError):
+    pass
 connection.commit()
-cursor.execute('SELECT * FROM names')
+
+rootdir = "/media/disk/Music/0-9"
+for root, dirs, files in os.walk(rootdir):
+    for name in files:
+        abspath = os.path.join(root, name)
+        relpath = os.path.relpath(abspath, rootdir)
+        statinfo = os.stat(abspath)
+        cursor.execute('INSERT INTO files VALUES (null, ?, ?, ?)', (relpath, statinfo.st_mtime, statinfo.st_size))
+        connection.commit()
+        #print name + ":",  statinfo.st_mtime, statinfo.st_size
+"""os.path.commonprefix(list), shutil.copy2 does metadata, os.stat().st_mtime"""
+sys.exit()
+
+
+
+
+
+
+
 
 for row in cursor:
-    print 'ID:    ', row[0]
-    print 'Name:  ', row[1]
-    print 'Email: ', row[2]
+    try:
+        print 'ID: ', row[0], 'Parent: ', row[1], 'path: ', row[3]
+    except (UnicodeEncodeError):
+        print 'Fucked up Unicode or something ****'
     
