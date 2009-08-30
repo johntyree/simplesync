61658ea5b0ba3ac49edd4b359880a0d601054905
John Tyree <johntyree@gmail.com>
2009-08-30 12:08:00 -0400

    Walks music dir and adds to db. Created class for db.

--- db.py
+++ db.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python
-
 from pysqlite2 import dbapi2 as sqlite
-import time, sys, os
-
-connection = sqlite.connect('/tmp/test.db')
-cursor = connection.cursor()
-try:
-    cursor.execute('CREATE TABLE files (id INTEGER PRIMARY KEY, path VARCHAR(255), timestamp DECIMAL(12), size INTEGER(12))')
-except (sqlite.OperationalError):
-    pass
-connection.commit()
-
-rootdir = "/media/disk/Music/0-9"
-for root, dirs, files in os.walk(rootdir):
-    for name in files:
-        abspath = os.path.join(root, name)
-        relpath = os.path.relpath(abspath, rootdir)
-        statinfo = os.stat(abspath)
-        cursor.execute('INSERT INTO files VALUES (null, ?, ?, ?)', (relpath, statinfo.st_mtime, statinfo.st_size))
-        connection.commit()
-        #print name + ":",  statinfo.st_mtime, statinfo.st_size
-"""os.path.commonprefix(list), shutil.copy2 does metadata, os.stat().st_mtime"""
-sys.exit()
+import time, sys, os, tagpy, subprocess
 
 
+class musicDB:
+    def __init__ (self, dbfile):
+        '''A database of file information and tag attributes'''
+        self.connection = sqlite.connect(dbfile)
+        self.cursor = self.connection.cursor()
+        for table in "file", "album", "artist", "genre":
+            self.cursor.execute('DROP TABLE IF EXISTS %s' % table)
+        self.cursor.execute('CREATE TABLE file (path VARCHAR(255) PRIMARY KEY, timestamp DECIMAL(12), size INTEGER(12), name VARCHAR(255), artist_id INTEGER, album_id INTEGER, genre_id INTEGER, year INTEGER, copy BOOLEAN)')
+        for table in "album", "artist", "genre":
+            self.cursor.execute('CREATE TABLE %s (id INTEGER PRIMARY KEY, name VARCHAR(255))' % table)
+        self.connection.commit()
+
+    def addDir(self, rootdir):
+        '''Recursively import a directory into the database'''
+        cursor = self.cursor
+        for root, dirs, files in os.walk(rootdir):
+            print root
+            for name in files:
+                if not '.mp3' in name[-4:]:
+                    continue
+                abspath = os.path.join(root, name)
+                relpath = unicode(os.path.relpath(abspath, rootdir), 'latin-1')
+                statinfo = os.stat(abspath)
+                f = tagpy.FileRef(abspath)
+                print f.tag().album
+                cursor.execute('SELECT id FROM artist WHERE name = "%s"' % (f.tag().artist,))
+                try:
+                    artist_id = cursor.fetchall()[0][0]
+                except (IndexError):
+                    cursor.execute('INSERT INTO artist VALUES (null, ?)', (f.tag().artist,))
+                    cursor.execute('SELECT id FROM artist WHERE name = "%s"' % (f.tag().artist,))
+                    artist_id = cursor.fetchall()[0][0]
+                cursor.execute('SELECT id FROM genre WHERE name = "%s"' % (f.tag().genre,))
+                try:
+                    genre_id = cursor.fetchall()[0][0]
+                except (IndexError):
+                    cursor.execute('INSERT INTO genre VALUES (null, ?)', (f.tag().genre,))
+                    cursor.execute('SELECT id FROM genre WHERE name = "%s"' % (f.tag().genre,))
+                    genre_id = cursor.fetchall()[0][0]
+                cursor.execute('SELECT id FROM album WHERE name = "%s"' % (f.tag().album,))
+                try:
+                    album_id = cursor.fetchall()[0][0]
+                except (IndexError):
+                    cursor.execute('INSERT INTO album VALUES (null, ?)', (f.tag().album,))
+                    cursor.execute('SELECT id FROM album WHERE name = "%s"' % (f.tag().album,))
+                    album_id = cursor.fetchall()[0][0]
+                cursor.execute('INSERT INTO file VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', (relpath, statinfo.st_mtime, statinfo.st_size, f.tag().title, artist_id, album_id, genre_id, f.tag().year, True))
+        self.connection.commit()
+
+def main():
+    #subprocess.call(["rm","/tmp/test.db"])
+    #connection = sqlite.connect('/tmp/test.db')
+    db = musicDB(":memory:") 
+    rootdir = "/media/disk/Music/S/Sneaky Sound System"
+    db.addDir(rootdir)
+
+
+if __name__ == "__main__": main()
 
 
 
 
-
-
-for row in cursor:
-    try:
-        print 'ID: ', row[0], 'Parent: ', row[1], 'path: ', row[3]
-    except (UnicodeEncodeError):
-        print 'Fucked up Unicode or something ****'
-    
+"""shutil.copy2 does metadata, os.stat().st_mtime"""
